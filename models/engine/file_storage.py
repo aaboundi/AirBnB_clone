abfa9c1e03601c2dcf2932ad34ecaed2e9036b22
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-08-06 23:28:16 +0000

    BaseModel3,5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,3 +27,14 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
+        
+    def reload(self):
+        """Deserializes JSON file into __objects."""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
