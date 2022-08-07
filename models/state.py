614d45a022e835070e1f1527f23e7294c2c599f4
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-08-07 16:49:49 +0000

    More Classes

--- models/state.py
+++ models/state.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Module for User class."""
+# state.py
+"""Module for State class."""
+
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """Class representing a User."""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class State(BaseModel):
+    """Class representing a State."""
+    name = ""
