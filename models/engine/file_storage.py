d4b79e75be1477377761818157d4d42c4e53325b
aaboundi <aadja@gmail.com>
2022-08-06 17:55:14 -0700

    BaseModel8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,7 +6,6 @@ import os
 
 
 class FileStorage:
-
     """Class for serializtion and deserialization of base classes."""
     __file_path = "file.json"
     __objects = {}
@@ -31,8 +30,10 @@ class FileStorage:
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
         from models.base_model import BaseModel
+        from models.user import User
 
-        classes = {"BaseModel": BaseModel}
+        classes = {"BaseModel": BaseModel,
+                   "User": User}
         return classes
 
     def reload(self):
@@ -52,6 +53,11 @@ class FileStorage:
             "BaseModel":
                      {"id": str,
                       "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime}
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first-name": str,
+                      "last_name": str}
                      }
         return attributes
