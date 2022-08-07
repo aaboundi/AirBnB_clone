6b1299af4657c7ae8a0729e4e1e6dc756a1a15a8
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-08-07 16:46:02 +0000

    More Classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Module for User class."""
+# amenity.py
+"""Module for Amenity class."""
+
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """Class representing a User."""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class Amenity(BaseModel):
+    """Class representing a Amenity."""
+    name = ""
